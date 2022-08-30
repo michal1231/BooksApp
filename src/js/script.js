eb9d8319b3941f98ce32b59f607723bbbdd3d3af
Michał Mietła <mrbroomchannel@gmail.com>
2022-08-30 19:21:02 +0200

    Add adding to favorite

--- src/js/script.js
+++ src/js/script.js
@@ -3,9 +3,10 @@
 const bookDisplay = Handlebars.compile(document.querySelector('#template-book').innerHTML);
 const bookListWrapper = document.querySelector('.books-list');
 
-function renderBooks() {
+const favoriteBooks = [];
 
-  console.log(bookListWrapper);
+
+function renderBooks() {
   for (let book of dataSource.books) {
     const generatedHTML = bookDisplay(book);
     const generatedDOM = utils.createDOMFromHTML(generatedHTML);
@@ -13,4 +14,17 @@ function renderBooks() {
   }
 }
 
+
+function initActions() {
+  const booksDOMs = document.querySelectorAll('.book__image');
+  for (let book of booksDOMs) {
+    book.addEventListener('dblclick', function (event) {
+      event.preventDefault();
+      favoriteBooks.push(book.getAttribute('data-id'));
+      book.classList.add('favorite');
+    });
+  }
+}
+
 renderBooks();
+initActions();
